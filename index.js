c87abeaab11911c13fd65c010ea740c584acbebc
zluigon <luis.nmn.gonzalez@gmail.com>
2024-09-09 13:18:43 -0400

    Update: Customer class complete w/ passing tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import Classes Here
 const Product = require("./classes/Product");
 const Cart = require("./classes/Cart");
+const Customer = require("./classes/Customer");
 // DO NOT EDIT BELOW THIS LINE
 try {
   module.exports = {
